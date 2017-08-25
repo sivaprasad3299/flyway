a7e84bf5c1be32011d33ed74126ca0a1883a7152
Siva Prasad <siva.prasad.narravula@hp.com>
2017-08-24 23:52:01 -0700

    Added titles table to payrool db

---
+++ sql/V002__Created Titles Table .sql
@@ -0,0 +1,9 @@
+CREATE TABLE titles(
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(100) NOT NULL,
+PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+INSERT INTO titles (title) VALUES('Underling');
+INSERT INTO titles (title) VALUES('Evil Overload');
+INSERT INTO titles (title) VALUES('Benevolent Dictator');
